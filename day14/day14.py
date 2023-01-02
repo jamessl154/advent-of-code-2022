013dc0ca7b9778fade50d1132c81d1dab0f20e5b
James Sue-Ling <james_sueling@hotmail.co.uk>
2023-01-02 18:50:10 +0000

    day14 refactor

--- day14/day14.py
+++ day14/day14.py
@@ -4,8 +4,8 @@ def day14():
   with open("input.txt", "r") as f:
     puzzle_input = f.read().split("\n")
   
-  rock_points_x = defaultdict(set)
-  rock_points_y = defaultdict(set)
+  p1_rock_points = defaultdict(set)
+  p2_rock_points = defaultdict(set)
 
   max_depth = float("-inf")
 
@@ -20,75 +20,50 @@ def day14():
 
       for j in range(min(from_x, to_x), max(from_x, to_x) + 1):
         for k in range(min(from_y, to_y), max(from_y, to_y) + 1):
-          rock_points_x[j].add(k)
-          rock_points_y[k].add(j)
+          p1_rock_points[k].add(j)
+          p2_rock_points[k].add(j)
 
   p1_rested_sand = 0
   p2_rested_sand = 0
 
-  def p1_drop_sand():
-    pos = [500, 0]
-
-    while pos[1] < max_depth:
-      down = pos[1] + 1
-      left = pos[0] - 1
-      right = pos[0] + 1
-      if pos[0] not in rock_points_x or down not in rock_points_x[pos[0]]:
-        pos[1] += 1
-      elif down not in rock_points_x[left]:
-        pos[0] -= 1
-        pos[1] += 1
-      elif down not in rock_points_x[right]:
-        pos[0] += 1
-        pos[1] += 1
-      else:
-        break
-    
-    # returns position of sand either settled or infinitely falling once below the max depth
-    return pos
-
   infinite_floor = max_depth + 2
 
-  def p2_drop_sand():
+  def drop_sand(rock_points, max_depth):
     pos = [500, 0]
-
     while True:
       down = pos[1] + 1
       left = pos[0] - 1
       right = pos[0] + 1
 
-      if down == infinite_floor:
-        break
+      if down == max_depth:
+        return pos
       
-      if pos[0] not in rock_points_y[down]:
+      if down not in rock_points or pos[0] not in rock_points[down]:
         pos[1] += 1
-      elif left not in rock_points_y[down]:
+      elif left not in rock_points[down]:
         pos[0] -= 1
         pos[1] += 1
-      elif right not in rock_points_y[down]:
+      elif right not in rock_points[down]:
         pos[0] += 1
         pos[1] += 1
       else:
-        break
-    
-    return pos
-  
-  while True:
-    x,y = p1_drop_sand()
+        return pos
 
-    if y >= max_depth:
+  while True:
+    x, y = drop_sand(p1_rock_points, max_depth + 1)
+
+    if y == max_depth:
       print("part1", p1_rested_sand)
       break
 
     # settled sand becomes rock
-    rock_points_x[x].add(y)
+    p1_rock_points[y].add(x)
     p1_rested_sand += 1
   
   while True:
-    x,y = p2_drop_sand()
+    x, y = drop_sand(p2_rock_points, infinite_floor)
 
-    # settled sand becomes rock
-    rock_points_y[y].add(x)
+    p2_rock_points[y].add(x)
     p2_rested_sand += 1
 
     if x == 500 and y == 0:
